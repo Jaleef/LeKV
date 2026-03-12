ca78620d7a601f15efae5ff64792174d768fcb5a
jeff <jeffyan167@gmail.com>
2026-03-12 16:16:07 +0800

    重写了leKV的启动模块, 由RPC_Server负责启动kv并接收客户端的请求

--- src/lekv.cpp
+++ src/lekv.cpp
@@ -1,22 +1,56 @@
 #include <iostream>
+#include <sstream>
 
-#include "storage_engine.h"
 #include "kv_server.h"
+#include "rpc_server.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "kv server启动" << std::endl;
+    KVServer kv;
 
-    StorageEngine sto = StorageEngine();
-    sto.put("key1", "value1");
-    std::string value;
-    sto.get("key1", value);
-    
-    KVServer server = KVServer();
-    server.put("key2", "value2");
-    server.get("key2", value);
-    
-    std::cout << "key1: " << value << std::endl;
-    std::cout << "key2: " << value << std::endl;
+    RpcServer server(9000);
+
+    server.register_handler("PUT", [&kv](const std::string& req) {
+        std::stringstream ss(req);
+
+        std::string cmd, key, value;
+
+        ss >> cmd >> key >> value;
+
+        kv.put(key, value);
+
+        return std::string("OK\n");
+    });
+
+    server.register_handler("GET", [&kv](const std::string& req) {
+        std::stringstream ss(req);
+
+        std::string cmd, key;
+
+        ss >> cmd >> key;
+
+        std::string value;
+        if (kv.get(key, value)) {
+            return value + "\n";
+        }
+
+        return std::string("NOT FOUND\n");
+    });
+
+    server.register_handler("DEL", [&kv](const std::string& req) {
+        std::stringstream ss(req);
+
+        std::string cmd, key;
+
+        ss >> cmd >> key;
+
+        if (kv.del(key)) {
+            return std::string("OK\n");
+        }
+
+        return std::string("NOT FOUND\n");
+    });
+
+    server.start();
 
     return 0;
 }
