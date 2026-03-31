41334c8b3951a86e77cf2b3f034f45846cef63ed
jeff <jeffyan167@gmail.com>
2026-03-31 12:31:57 +0800

    优化maing函数启动文件

--- src/lekv.cpp
+++ src/lekv.cpp
@@ -1,13 +1,26 @@
-#include <iostream>
-#include <sstream>
+#include "raft_node.h"
 
-#include "kv_server.h"
-#include "rpc_server.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
-    KVServer kv(8001);
+    // 默认端口
+    uint16_t port = 9001;
 
-    kv.Start();
+    // 使用 --port 指定端口
+    for (int i = 1 ; i < argc ; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--port" && i + 1 < argc) {
+            port = static_cast<uint16_t>(std::stoi(argv[++i]));
+        }
+    }
+
+    RaftNode node(port);
+
+    std::cout << "Starting LEKV..." << std::endl;
+    std::cout << "Commands: PUT k v | GET k | DELETE k" << std::endl;
+
+    node.Run();
 
     return 0;
 }
